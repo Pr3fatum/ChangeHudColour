8ad4920155ff3eca26f5dffc7ccba557fac3d820
Pr3fatum <evilmercenary@gmail.com>
2017-07-05 12:48:17 +0100

    adding backup/restore functions to the basic script and improving errors

--- ChangeHudColour.cmd
+++ ChangeHudColour.cmd
@@ -2,27 +2,48 @@
 echo ================================
 echo ChangeHudColour
 echo Scripted By Ev0l
-echo USAGE: select red, blue, green or default
+echo USAGE: select red, blue, green or default for colours.
+echo        for backup/restore functions, enter "backup" or "restore"
 echo ================================
 
+:select
 set FILE = "%userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics\GraphicsConfigurationOverride.xml"
-echo " ENTER COLOUR FROM OPTIONS ABOVE (red, blue, green or default): "
+echo " ENTER COLOUR FROM OPTIONS ABOVE (red, blue, green or default) or the following commands (backup or restore): "
 set /p HUDCOLOUR=
-
-echo [-] Finding Directory
-cd %userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics || echo DIRECTORY NOT THERE 
-echo [-] removing file
-if exist GraphicsConfigurationOverride.xml del GraphicsConfigurationOverride.xml
 
 REM echo colour entered is "%HUDCOLOUR%"
 if "%HUDCOLOUR%" == "red" goto red 
 if "%HUDCOLOUR%" == "blue" goto blue
 if "%HUDCOLOUR%" == "green" goto green
 if "%HUDCOLOUR%" == "default" goto default
+if "%HUDCOLOUR%" == "backup" goto backup
+if "%HUDCOLOUR%" == "restore" goto restore
 goto nocolourmatched
+
+:backup
+echo SELECTED BACKUP
+echo [-] Copying existing GraphicsConfigurationOverride.xml File to backup location
+copy "%userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics\GraphicsConfigurationOverride.xml" "%userprofile%\GraphicsConfigurationOverride.xml.bkp" || echo CANNOT BACKUP FILE && goto end
+echo [-] Backup Complete -  please find your backup in the directory "%userprofile%\GraphicsConfigurationOverride.xml.bkp"
+echo --- to restore this file at a later date use the command restore.
+goto end
+
+:restore
+echo SELECTED RESTORE
+cd %userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics || echo ED GRAPHICS DIRECTORY DOES NOT EXIST OR INACCESSIBLE && goto end
+echo [-] Removing Existing XML File
+if exist GraphicsConfigurationOverride.xml del GraphicsConfigurationOverride.xml || echo CANNOT DELETE ORIGINAL XML FILE && goto end
+echo [-] Copying existing GraphicsConfigurationOverride.xml File from previous backup to live location
+copy "%userprofile%\GraphicsConfigurationOverride.xml.bkp" "%userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics\GraphicsConfigurationOverride.xml" || echo CANNOT RESTORE FILE && goto end
+echo [-] Restore Complete -  If your game is running, please restart it for the changes to take effect
+goto end
 
 :red
 echo SELECTED RED
+echo [-] Finding Directory
+cd %userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics || echo ED GRAPHICS DIRECTORY DOES NOT EXIST OR INACCESSIBLE && goto end
+echo [-] Removing Original File
+if exist GraphicsConfigurationOverride.xml del GraphicsConfigurationOverride.xml || echo CANNOT DELETE ORIGIN XML FILE
 echo [-] Creating New GraphicsConfigurationOverride.xml File
 echo ^<?xml version=^"1.0^" encoding=^"UTF-8^" ?^> > GraphicsConfigurationOverride.xml
 echo ^<GraphicsConfig^> >> GraphicsConfigurationOverride.xml
@@ -41,6 +62,10 @@ goto end
 
 :green
 echo SELECTED GREEN
+echo [-] Finding Directory
+cd %userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics || echo ED GRAPHICS DIRECTORY DOES NOT EXIST OR INACCESSIBLE && goto end
+echo [-] Removing Original File
+if exist GraphicsConfigurationOverride.xml del GraphicsConfigurationOverride.xml || echo CANNOT DELETE ORIGIN XML FILE
 echo [-] Creating New GraphicsConfigurationOverride.xml File
 echo ^<?xml version=^"1.0^" encoding=^"UTF-8^" ?^> > GraphicsConfigurationOverride.xml
 echo ^<GraphicsConfig^> >> GraphicsConfigurationOverride.xml
@@ -58,6 +83,10 @@ goto end
 
 :blue
 echo SELECTED BLUE
+echo [-] Finding Directory
+cd %userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics || echo ED GRAPHICS DIRECTORY DOES NOT EXIST OR INACCESSIBLE && goto end
+echo [-] Removing Original File
+if exist GraphicsConfigurationOverride.xml del GraphicsConfigurationOverride.xml || echo CANNOT DELETE ORIGIN XML FILE
 echo [-] Creating New GraphicsConfigurationOverride.xml File
 echo ^<?xml version=^"1.0^" encoding=^"UTF-8^" ?^> > GraphicsConfigurationOverride.xml
 echo ^<GraphicsConfig^> >> GraphicsConfigurationOverride.xml
@@ -75,6 +104,10 @@ goto end
 
 :default
 echo SELECTED DEFAULT
+echo [-] Finding Directory
+cd %userprofile%\appdata\local\Frontier Developments\Elite Dangerous\Options\Graphics || echo ED GRAPHICS DIRECTORY DOES NOT EXIST OR INACCESSIBLE && goto end
+echo [-] Removing Original File
+if exist GraphicsConfigurationOverride.xml del GraphicsConfigurationOverride.xml || echo CANNOT DELETE ORIGIN XML FILE
 echo [-] Creating New GraphicsConfigurationOverride.xml File
 echo ^<?xml version=^"1.0^" encoding=^"UTF-8^" ?^> > GraphicsConfigurationOverride.xml
 echo ^<GraphicsConfig^> >> GraphicsConfigurationOverride.xml
@@ -84,7 +117,12 @@ goto end
 
 :nocolourmatched
 echo you entered an invalid colour. please try again.
+echo . 
+goto select
 
+:writeerror
+echo UNABLE TO WRITE TO GraphicsConfigurationOverride.xml
+goto end
 
 :end
 pause
